2bc4df211837e5bd8d1d50c1cd80993e27843e89
Alex Bakun <bakun.alex93@gmail.com>
2022-04-17 17:32:54 +0300

    forgot password setup email

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,52 +1,61 @@
 import React from "react";
-import { Form, Formik } from "formik";
+import { Formik, Form } from "formik";
 import { Box, Button } from "@chakra-ui/react";
-import Wrapper from "../components/Wrapper";
-import InputField from "../components/InputField";
-import { useRegisterMutation } from "../generated/graphql";
-import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
 import { withUrqlClient } from "next-urql";
+
+import { toErrorMap } from "../utils/toErrorMap";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { useRegisterMutation } from "../generated/graphql";
 
-interface IRegisterProps {}
+import InputField from "../components/InputField";
+import Wrapper from "../components/Wrapper";
 
-const Register: React.FC<IRegisterProps> = ({}) => {
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
   const router = useRouter();
-  const [{}, register] = useRegisterMutation();
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ email: "", username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
+          const response = await register({ options: values });
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
           } else if (response.data?.register.user) {
-            await router.push("/");
+            // worked
+            router.push("/");
           }
         }}
       >
         {({ isSubmitting }) => (
           <Form>
             <InputField
-              label="Username"
               name="username"
               placeholder="username"
+              label="Username"
             />
             <Box mt={4}>
+              <InputField name="email" placeholder="email" label="Email" />
+            </Box>
+            <Box mt={4}>
               <InputField
-                label="Password"
                 name="password"
                 placeholder="password"
+                label="Password"
                 type="password"
               />
             </Box>
-            <Box mt={4}>
-              <Button type="submit" colorScheme="teal" isLoading={isSubmitting}>
-                register
-              </Button>
-            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              register
+            </Button>
           </Form>
         )}
       </Formik>
