a4c7815d1d89edce82e8056cb2341107f72733c7
Alex Bakun <bakun.alex93@gmail.com>
2022-04-17 11:11:18 +0300

    refactoring, session more info, nextjs chakra, urql basic, graphql codegen, register errors

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import {Form, Formik} from "formik";
-import {Box, Button,} from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface IRegisterProps {}
 
 const Register: React.FC<IRegisterProps> = ({}) => {
+  const router = useRouter();
+  const [{}, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            await router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
