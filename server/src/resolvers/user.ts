35f2132ecdd3df9745f1eca6a8f21850e3c9755f
Alex Bakun <bakun.alex93@gmail.com>
2022-04-18 10:24:41 +0300

    start change password

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,10 +11,11 @@ import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 import { EntityManager } from "@mikro-orm/postgresql";
-import { COOKIE_NAME } from "../constants";
+import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from "../constants";
 import { UsernamePasswordInput } from "./UsernamePasswordInput";
 import { validateRegister } from "../utils/validateRegister";
 import { sendEmail } from "../utils/sendEmail";
+import { v4 } from "uuid";
 
 @ObjectType()
 class FieldError {
@@ -36,14 +37,24 @@ class UserResponse {
 @Resolver()
 export class UserResolver {
   @Mutation(() => Boolean)
-  async forgotPassword(@Arg("email") email: string, @Ctx() { em }: MyContext) {
+  async forgotPassword(
+    @Arg("email") email: string,
+    @Ctx() { em, redis }: MyContext
+  ) {
     const user = await em.findOne(User, { email });
     if (!user) {
       //the email is not in db
       return true;
     }
 
-    const token = "1token1";
+    const token = v4();
+
+    await redis.set(
+      FORGET_PASSWORD_PREFIX + token,
+      user.id,
+      "PX",
+      1000 * 60 * 60 * 24 * 3 // 3 days
+    );
 
     await sendEmail(
       email,
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 
 import theme from "../theme";
 
+// @ts-ignore
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS theme={theme}>
